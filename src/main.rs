7ac01ca6dcae99d04832557dda17acfdbf45106f
hooneun <kimhoon0316@gmail.com>
2021-11-19 22:59:37 +0900

    actix, tera add default code write

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use tera::{Context, Tera};
+
+async fn index(tera: web::Data<Tera>) -> impl Responder {
+    let mut data = Context::new();
+    data.insert("title", "Hacker Clone");
+    data.insert("name", "Hooneun");
+
+    let rendered = tera.render("index.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let tera = Tera::new("templates/**/*").unwrap();
+    HttpServer::new(move || {
+        App::new()
+            .data(tera.clone())
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
